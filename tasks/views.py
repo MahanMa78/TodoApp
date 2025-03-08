0c429ce3b025737070869c3289a58bf6481148e3
Mahan Mazaheri <mahan78ir@gmail.com>
2025-03-08 12:19:43 +0330

    Create TaskForm and add to views and tasks.html

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+from .models import Task
+from .forms import TaskForm
 
 def index(request):
     
+    form = TaskForm()
     
-    return render(request , 'tasks.html')
+    tasks = Task.objects.all()
+    
+    context = {'tasks':tasks , "TaskForm" : form}
+    
+    return render(request , 'tasks.html',context )
