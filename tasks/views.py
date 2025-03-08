cddb16cb5169fa4a32a9f564e555198db3634d5c
Mahan Mazaheri <mahan78ir@gmail.com>
2025-03-08 12:39:44 +0330

    change method post to POST in views and tasks.html

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from django.http import HttpResponse
 from .models import Task
 from .forms import TaskForm
@@ -9,6 +9,15 @@ def index(request):
     
     tasks = Task.objects.all()
     
+    if request.method == 'POST':
+        
+        form = TaskForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+        
+        return redirect('/')        
+                
     context = {'tasks':tasks , "TaskForm" : form}
     
     return render(request , 'tasks.html',context )
