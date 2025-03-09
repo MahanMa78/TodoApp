aed95530814f02fe43de3332be21aac016cc4caf
Mahan Mazaheri <mahan78ir@gmail.com>
2025-03-09 09:49:47 +0330

    Add DeleteTask feature to project

--- tasks/urls.py
+++ tasks/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('',views.index, name=""),
     path('update-task/<str:pk>/',views.updateTask , name="update-task"),
+    path('delete-task/<str:pk>/',views.deleteTask , name="delete-task"),
     ]
