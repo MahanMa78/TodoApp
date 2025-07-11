5aa16184779299e184878b355650fd15df2db540
Mahan Mazaheri <mahan78ir@gmail.com>
2025-07-11 08:25:18 +0330

    add test to Tasks

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Task
 
-# Create your tests here.
+
+class TaskModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.task = Task.objects.create(
+            title = "First task",
+            body = "A body of text here",
+        )
+        
+    def test_model_content(self):
+        self.assertEqual(self.task.title,"First task")
+        self.assertEqual(self.task.body , "A body of text here")
+        self.assertEqual(str(self.task),"First task")
