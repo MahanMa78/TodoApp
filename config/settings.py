85c3e29b819cb7ccdbb9040e37eb0a805e834523
Mahan Mazaheri <mahan78ir@gmail.com>
2025-07-11 16:54:59 +0330

    create CustomUser and configure for users

--- config/settings.py
+++ config/settings.py
@@ -155,3 +155,4 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = "accounts.CustomUser"
