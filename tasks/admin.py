136642d47721b6e99c095a0caf6e6fa11eb85c95
Mahan Mazaheri <mahan78ir@gmail.com>
2025-07-11 08:10:34 +0330

    rewrite admin

--- tasks/admin.py
+++ tasks/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 
 from .models import Task
 
-admin.site.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display=["title" , 'body']
+
+
+admin.site.register(Task , TaskAdmin)
