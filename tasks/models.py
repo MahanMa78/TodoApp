d13f570ef5491e44813f21bd86dfdd78e039cba3
Mahan Mazaheri <mahan78ir@gmail.com>
2025-07-11 18:27:09 +0330

    add author for tasks

--- tasks/models.py
+++ tasks/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.conf import settings
 
 class Task(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL , on_delete=models.CASCADE ,related_name='tasks' , default=None )
     title = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
