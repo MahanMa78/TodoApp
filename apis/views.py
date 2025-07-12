4772ad668fee1c8cf0673ec528d0d83c664da883
Mahan Mazaheri <mahan78ir@gmail.com>
2025-07-12 11:20:46 +0330

    create special permission in permissions.py and use that in views.py

--- apis/views.py
+++ apis/views.py
@@ -2,12 +2,16 @@ from rest_framework import generics , permissions
 from tasks.models import Task
 from .serializers import TaskSerializer
 
+from .permissions import IsAuthorOrReadOnly
+
 class ListTask(generics.ListCreateAPIView): # * az ListAPIView be ListCreateAPIView taghir midahim baraye inke ghabeliat hay read-write ro dashte bashim
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
     
 class DetailTask(generics.RetrieveUpdateDestroyAPIView): # * az RetrieveAPIView be RetrieveUpdateDestroyAPIView taghir midim baraye read, updated, or deleted
-    permission_classes = (permissions.IsAuthenticated , )
+    # permission_classes = (permissions.IsAuthenticated , ) #TODO ---> in marbot be khode django hast
+    permission_classes = (IsAuthorOrReadOnly ,) #! in ro ma khodemon sakhtim ---> faghat on ke chizi neveshte mitone taghirat bede
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
 
