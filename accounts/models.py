8ca97a5c3c5965dbfdff7bc9c6dbbaef86fcd745
Mahan Mazaheri <mahan78ir@gmail.com>
2025-07-12 22:59:17 +0330

    add user authentication

--- accounts/models.py
+++ accounts/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     birth_date = models.DateField(null = True , blank=True)
+    phone_number = models.CharField(max_length=20 , null=True , blank=True)
     
     def __str__(self):
         return self.username
--- apis/serializers.py
+++ apis/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from tasks.models import Task
+from dj_rest_auth.registration.serializers import RegisterSerializer
+from accounts.models import CustomUser
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
@@ -12,3 +14,20 @@ class TaskSerializer(serializers.ModelSerializer):
             'completed',
             'created'
         )
+        
+        
+# class CustomRegisterSerializer(RegisterSerializer):
+#     birth_date = serializers.DateField(required=False)
+#     phone_number = serializers.CharField(required = False)
+    
+#     def get_cleaned_data(self):
+#         data = super().get_cleaned_data()
+#         data['birth_date'] = self.validated_data.get('birth_date', '')
+#         data['phone_number'] = self.validated_data.get('phone_number' , '')
+#         return data
+
+class CustomRegisterSerializer(RegisterSerializer):
+    class Meta:
+        model = CustomUser
+        fields = "__all__"
+    
