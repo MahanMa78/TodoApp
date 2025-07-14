ee7b804ffcbcfab082d741f956eb27f51ec31bce
Mahan Mazaheri <mahan78ir@gmail.com>
2025-07-14 14:43:49 +0330

    add viewset and router to project

--- apis/urls.py
+++ apis/urls.py
@@ -1,11 +1,19 @@
 from django.urls import path
-from .views import DetailTask , ListTask , UserList , UserDetail
+# from .views import DetailTask , ListTask , UserList , UserDetail
 
 
-urlpatterns = [
-    path("<int:pk>/" , DetailTask.as_view() ,name="task_detail" ),
-    path("" , ListTask.as_view() , name='task_list'),
-    path("users/" , UserList.as_view() , name="user_list"),
-    path("users/<int:pk>/" , UserDetail.as_view() , name="user_detail"),
+# urlpatterns = [
+#     path("<int:pk>/" , DetailTask.as_view() ,name="task_detail" ),
+#     path("" , ListTask.as_view() , name='task_list'),
+#     path("users/" , UserList.as_view() , name="user_list"),
+#     path("users/<int:pk>/" , UserDetail.as_view() , name="user_detail"),
     
-]
+# ]
+from rest_framework.routers import SimpleRouter
+from .views import UserViewSet , TaskViewSet
+
+router = SimpleRouter()
+router.register("users" , UserViewSet , basename='users')
+router.register("" , TaskViewSet , basename="tasks")
+
+urlpatterns = router.urls
--- apis/views.py
+++ apis/views.py
@@ -2,26 +2,40 @@ from rest_framework import generics , permissions
 from tasks.models import Task
 from .serializers import TaskSerializer , UserSerializer
 from django.contrib.auth import get_user_model 
+from rest_framework import viewsets
 
 from .permissions import IsAuthorOrReadOnly
 
 #* yadam bashe baadan baraash tarif konam ke faghat task haie ke on shakhs neveshte ro namayesh bede
-class ListTask(generics.ListCreateAPIView): # * az ListAPIView be ListCreateAPIView taghir midahim baraye inke ghabeliat hay read-write ro dashte bashim
+# class ListTask(generics.ListCreateAPIView): # * az ListAPIView be ListCreateAPIView taghir midahim baraye inke ghabeliat hay read-write ro dashte bashim
+#     permission_classes = (IsAuthorOrReadOnly,)
+#     queryset = Task.objects.all()
+#     serializer_class = TaskSerializer
+    
+# class DetailTask(generics.RetrieveUpdateDestroyAPIView): # * az RetrieveAPIView be RetrieveUpdateDestroyAPIView taghir midim baraye read, updated, or deleted
+#     # permission_classes = (permissions.IsAuthenticated , ) #TODO ---> in marbot be khode django hast
+#     permission_classes = (IsAuthorOrReadOnly ,) #! in ro ma khodemon sakhtim ---> faghat on ke chizi neveshte mitone taghirat bede
+#     queryset = Task.objects.all()
+#     serializer_class = TaskSerializer
+
+# class UserList(generics.ListCreateAPIView):
+#     queryset = get_user_model().objects.all()
+#     serializer_class = UserSerializer
+    
+    
+# class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = get_user_model().objects.all()
+#     serializer_class = UserSerializer
+
+
+# ! viewsets:
+class TaskViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthorOrReadOnly,)
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
     
-class DetailTask(generics.RetrieveUpdateDestroyAPIView): # * az RetrieveAPIView be RetrieveUpdateDestroyAPIView taghir midim baraye read, updated, or deleted
-    # permission_classes = (permissions.IsAuthenticated , ) #TODO ---> in marbot be khode django hast
-    permission_classes = (IsAuthorOrReadOnly ,) #! in ro ma khodemon sakhtim ---> faghat on ke chizi neveshte mitone taghirat bede
-    queryset = Task.objects.all()
-    serializer_class = TaskSerializer
-
-class UserList(generics.ListCreateAPIView):
+    
+class UserViewSet(viewsets.ModelViewSet):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
     
-    
-class UserDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = get_user_model().objects.all()
-    serializer_class = UserSerializer
